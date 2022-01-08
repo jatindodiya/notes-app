793b37a04b9e8f4ce4334e76aeefb7229ba627a5
Jatin Dodiya <jatin.dodiya@gupshup.io>
2022-01-08 19:19:27 +0530

    remove notes

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const chalk = require("chalk")
 
 const getNotes = function () {
     return 'Your notes...'
@@ -43,7 +44,14 @@ const removeNotes = function(title){
     const notesToKeep = notes.filter(function(note){
         return note.title !== title
     })
-    saveNotes(notesToKeep)
+
+    if(notes.length > notesToKeep.length){
+        console.log(chalk.green.inverse("Notes removed!"))
+        saveNotes(notesToKeep)
+    }
+    else{
+        console.log(chalk.red.inverse("No note found"))
+    }
 }
 
 const showAllNotes = function(){
